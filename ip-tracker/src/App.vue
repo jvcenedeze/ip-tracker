cfa48f5526371cb93f5d7813b7c70d5e35804adb
poiozex <jvcenedeze@hotmail.com>
2021-08-18 19:33:41 -0300

    Added background image

--- ip-tracker/src/App.vue
+++ ip-tracker/src/App.vue
@@ -33,7 +33,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap");
 
 #app {
   font-family: "Rubik", sans-serif;
--- ip-tracker/src/components/Home.vue
+++ ip-tracker/src/components/Home.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="container-fluid">
-    <div class="container-fluid blue-background">
-      <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4">
-          <div class="d-flex flex-column justify-content-center">
-            <h3 class="text-center pt-3 header-h3">IP Address Tracker</h3>
-            <div class="input-group my-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Search for any IP address or domain"
-                aria-label="Search for any IP address or domain"
-                aria-describedby="basic-addon2"
-              >
-              <span
-                class="input-group-text"
-                id="basic-addon2"
-              ><i class="fas fa-angle-right"></i></span>
+    <div class="d-flex justify-content-center">
+      <div class="blue-background">
+        <div class="row">
+          <div class="col-4"></div>
+          <div class="col-4">
+            <div class="d-flex flex-column justify-content-center">
+              <h3 class="text-center pt-4 header-h3">IP Address Tracker</h3>
+              <div class="input-group my-3">
+                <input
+                  type="text"
+                  class="form-control shadow-none"
+                  placeholder="Search for any IP address or domain"
+                  aria-label="Search for any IP address or domain"
+                  aria-describedby="basic-addon2"
+                >
+                <span
+                  class="input-group-text d-flex justify-content-center alig-items-center"
+                  id="basic-addon2"
+                ><i class="fas fa-chevron-right fa"></i></span>
+              </div>
             </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+            <div class="data-status"></div>
           </div>
         </div>
       </div>
@@ -34,14 +42,29 @@ export default {
 
 <style lang="scss">
 .blue-background {
-  background-color: blue;
+  background-image: url("../assets/pattern-bg.png");
+  height: 250px;
+  width: 1440px;
   .header-h3 {
     color: white;
   }
   span#basic-addon2 {
     background-color: hsl(0, 0%, 17%);
+    border: none;
+    width: 50px;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
     i {
       color: white;
+    }
+  }
+  .input-group {
+    height: 50px;
+    input.form-control {
+      border: none;
+      border-top-left-radius: 15px;
+      border-bottom-left-radius: 15px;
+      padding-left: 20px;
     }
   }
 }
